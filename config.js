bdd74d519a132b06ea6c40b5f539f6b81c0f62f9
Tim Phillips <tim.p.phillips@gmail.com>
2017-03-15 15:09:31 -0700

    update db api url

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config.db = {}
 config.s3 = {}
 config.voscast = {}
 
-config.db.apiUrl = 'https://kchung-newsbody-api.now.sh'
+config.db.apiUrl = 'https://kchung-newsbody-db-api.now.sh'
 config.s3.rootUrl = 'http://archive.kchung.news/'
 config.voscast.url= 'http://s2.voscast.com:9208/;&type=mp3'
 
