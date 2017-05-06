ef97e03ce4445ea86fb6e49c01db80868004949f
Tim Phillips <tim.p.phillips@gmail.com>
2017-05-05 18:34:24 -0700

    remove header stylings

--- components/header.js
+++ components/header.js
@@ -1,21 +1,11 @@
-import React from 'react'
-
 export default () => (
   <div className='kchung'>
 
     <h1>KCHUNG NEWS BODY</h1>
 
     <style jsx> {`
-      .kchung {
-        display: flex;
-        justify-content: center;
-        margin: 25px;
-        width: 100%;
-      }
       h1 {
-        font-size: 4rem;
-        font-weight: normal;
-        letter-spacing: 7px;
+        letter-spacing: 0.25rem;
       }
     `}</style>
 
