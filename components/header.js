ca2c8a59831f36ac5abc0a3714537d3e027dbab0
Tim Phillips <tim.p.phillips@gmail.com>
2017-05-05 19:39:45 -0700

    transform title

--- components/header.js
+++ components/header.js
@@ -1,10 +1,11 @@
 export default () => (
   <div className='kchung'>
 
-    <h1>KCHUNG NEWS BODY</h1>
+    <h1>KCHUNG News Body</h1>
 
     <style jsx> {`
       h1 {
+        text-transform: lowercase;
         letter-spacing: 0.25rem;
       }
     `}</style>
