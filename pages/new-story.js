9d15baf4a9edd5dcd15fa0a93525b0952641d241
Tim Phillips <tim.p.phillips@gmail.com>
2017-08-28 12:22:38 -0700

    pass session to story form

--- pages/new-story.js
+++ pages/new-story.js
@@ -1,6 +1,6 @@
 import SecurePage from '../components/secure-page'
 import StoryForm from '../components/story-form'
 
-const NewStory = () => <StoryForm />
+const NewStory = ({ session }) => <StoryForm session={session} />
 
 export default SecurePage(NewStory)
