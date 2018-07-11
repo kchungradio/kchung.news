56e2c9dbc4b2b27bbe64d4b7efdac2698f0b7ccd
Tim Phillips <tim.p.phillips@gmail.com>
2018-07-10 17:40:51 -0700

    comment

--- components/page.js
+++ components/page.js
@@ -2,5 +2,11 @@ import injectSession from './injectSession'
 import wrapWithLayout from './wrapWithLayout'
 import compose from '../lib/compose'
 
-// some middleware we use on every page
+/*
+ * some middleware we use on every page
+ *
+ * gets called like `injectSession(wrapWithLayout(Page))`
+ * so it's really a flow, not a compose
+ */
+
 export default compose(injectSession, wrapWithLayout)
