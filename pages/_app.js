c18ebe34a8cfb2e148b12585e04f2c0bd6639e59
Tim Phillips <tim.p.phillips@gmail.com>
2018-07-19 09:54:02 -0700

    move player to app component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import App, { Container } from 'next/app'
 
+import Player from '../components/player'
+
 export default class extends App {
+  state = {
+    playing: false,
+    playerStory: {}
+  }
+
   static async getInitialProps ({ Component, router, ctx }) {
     let pageProps = {}
 
@@ -12,12 +19,36 @@ export default class extends App {
     return { pageProps }
   }
 
+  setPlayState = state => this.setState({ playing: state })
+
+  togglePlayPause = () => this.setState(prevState => ({
+    playing: !prevState.playing
+  }))
+
+  onStoryPlayClick = story => this.setState({
+    playing: true,
+    playerStory: story
+  })
+
   render () {
     const { Component, pageProps } = this.props
+    const { playing, playerStory } = this.state
 
     return (
       <Container>
-        <Component {...pageProps} />
+        <Component
+          {...pageProps}
+          onStoryPlayClick={this.onStoryPlayClick}
+        />
+        {Object.keys(playerStory).length > 0 && (
+          <Player
+            audioUrl={playerStory.audio && playerStory.audio.publicUrl}
+            title={playerStory.title}
+            playing={playing}
+            setPlayState={this.setPlayState}
+            togglePlayPause={this.togglePlayPause}
+          />
+        )}
       </Container>
     )
   }
