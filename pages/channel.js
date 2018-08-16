07d0a79651f068f57b179cd538e78bff693e900b
Tim Phillips <tim.p.phillips@gmail.com>
2018-08-16 09:27:39 -0700

    convert to graphql query

--- pages/channel.js
+++ pages/channel.js
@@ -1,78 +1,79 @@
-import { Component } from 'react'
-import request from 'axios'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
 
 import { Router } from '../routes'
 import Page from '../components/hoc/page'
 import Story from '../components/story'
-import config from '../config'
 
-class NewsBody extends Component {
-  static async getInitialProps ({ query }) {
-    // query.authorSlug comes from url defined in ../routes.js
-    const { authorSlug } = query
-
-    // set request parameters based on query
-    let options = {}
-    if (authorSlug) options.params = { authorSlug }
-
-    // get stories from api
-    const res = await request.get(config.api.storiesUrl, options)
-
-    // sort by date descending
-    const stories = res.data.slice().sort((a, b) =>
-      new Date(b.publishedAt) - new Date(a.publishedAt)
-    )
-
-    return { authorSlug, stories }
-  }
-
-  render () {
-    const {
-      session,
-      authorSlug,
-      stories,
-      onStoryPlayClick
-    } = this.props
-
-    const isHome = !authorSlug
-    const isUsersPage = session && (authorSlug === session.slug)
-    const noStories = stories && stories.length === 0
-
-    return (
-      <div>
-
-        {session && (isHome || isUsersPage) && (
-          <div>
-            <button onClick={() => Router.pushRoute('new-story')}>
-              New Story
-            </button>
-            <button onClick={() => Router.pushRoute('edit-profile')}>
-              Edit Profile
-            </button>
-          </div>
-        )}
-
-        <div>
-          {noStories && <p>No stories here...</p>}
-          {stories.map(story => (
-            <Story
-              key={`${story.authorId}_${story.createdAt}`}
-              story={story}
-              isUsersStory={session && session.id === story.authorId}
-              onPlayClick={onStoryPlayClick}
-            />
-          ))}
-        </div>
-
-        <style jsx>{`
-          button {
-            margin: 0 10px 10px 0;
-          }
-        `}</style>
-
-      </div>
-    )
-  }
+Channel.getInitialProps = async ({ query: { authorSlug } }) => {
+  // query.authorSlug comes from url defined in ../routes.js
+  return { authorSlug }
 }
 
-export default Page(NewsBody)
+function Channel ({
+  session,
+  authorSlug,
+  onStoryPlayClick
+}) {
+  const isHome = !authorSlug
+  const isUsersPage = session && (authorSlug === session.slug)
+
+  return (
+    <div>
+      {session && (isHome || isUsersPage) && (
+        <div>
+          <button onClick={() => Router.pushRoute('new-story')}>
+            New Story
+          </button>
+          <button onClick={() => Router.pushRoute('edit-profile')}>
+            Edit Profile
+          </button>
+        </div>
+      )}
+
+      <Query query={authorStories} variables={{ slug: authorSlug }}>
+        {({ loading, error, data }) => {
+          if (error) return <div className='error'>Error loading stories.</div>
+          if (loading) return <div>Loading...</div>
+
+          const stories = data.storiesByAuthorSlug
+
+          if (!stories.length) return <div>No stories here...</div>
+
+          return stories.map(story => (
+            <Story
+              key={story.id}
+              story={story}
+              isUsersStory={session && session.id === story.author.id}
+              onPlayClick={onStoryPlayClick}
+            />
+          ))
+        }}
+      </Query>
+
+      <style jsx>{`
+        button {
+          margin: 0 10px 10px 0;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+const authorStories = gql`
+  query StoriesByAuthorSlug ($slug: String!) {
+    storiesByAuthorSlug (slug: $slug) {
+      id
+      title
+      slug
+      description
+      location
+      publishedAt
+      audio { filename }
+      images { filename }
+      author { id, name }
+    }
+  }
+`
+
+export default Page(Channel)
