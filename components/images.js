6235f605196b40105a6bc73f1c43ead53b3f1226
Tim Phillips <3578574+tim-phillips@users.noreply.github.com>
2018-11-25 12:35:38 -0800

    Image lightbox (#54)
    * add lightbox
    * constrain images

--- components/images.js
+++ components/images.js
@@ -1,29 +1,103 @@
+import React, { Component } from 'react'
+import Lightbox from 'react-images'
+
 import config from '../config'
 
-export default ({ images, onDelete }) => (
-  <div>
-    {images &&
-      images.map(image => (
-        <div key={image.filename}>
-          <img src={config.s3.rootUrl + image.filename} />
-          {onDelete && <span onClick={() => onDelete(image.id)}>&times;</span>}
-        </div>
-      ))}
+export default class Images extends Component {
+  state = {
+    lightboxIsOpen: false,
+    currentImage: 0
+  }
 
-    <style jsx>{`
-      div {
-        display: inline-block;
-        position: relative;
-      }
-      img {
-        width: 150px;
-      }
-      span {
-        position: absolute;
-        top: 0;
-        right: 0;
-        cursor: pointer;
-      }
-    `}</style>
-  </div>
-)
+  openLightbox = i => {
+    this.setState({
+      lightboxIsOpen: true,
+      currentImage: i
+    })
+  }
+  closeLightbox = () => {
+    this.setState({
+      lightboxIsOpen: false
+    })
+  }
+
+  prevImage = () => {
+    this.setState(prevState => ({ currentImage: prevState.currentImage - 1 }))
+  }
+  nextImage = () => {
+    this.setState(prevState => ({ currentImage: prevState.currentImage + 1 }))
+  }
+
+  render () {
+    const { images, onDelete } = this.props
+    const { lightboxIsOpen, currentImage } = this.state
+
+    if (!images || !Array.isArray(images)) return null
+
+    const imageObjects = images.map(image => ({
+      src: config.s3.rootUrl + image.filename
+    }))
+
+    return (
+      <React.Fragment>
+        <ul>
+          {images.map((image, idx) => (
+            <li
+              key={image.filename + idx}
+              onClick={() => this.openLightbox(idx)}
+            >
+              <img src={config.s3.rootUrl + image.filename} />
+              {onDelete && (
+                <span
+                  onClick={e => {
+                    e.stopPropagation()
+                    onDelete(image.id)
+                  }}
+                >
+                  &times;
+                </span>
+              )}
+            </li>
+          ))}
+        </ul>
+
+        <Lightbox
+          images={imageObjects}
+          isOpen={lightboxIsOpen}
+          onClose={this.closeLightbox}
+          currentImage={currentImage}
+          onClickPrev={this.prevImage}
+          onClickNext={this.nextImage}
+          backdropClosesModal
+        />
+
+        <style jsx>{`
+          ul,
+          li {
+            padding: 0;
+            margin: 0;
+          }
+          ul {
+            overflow-y: hidden;
+            white-space: nowrap;
+          }
+          li {
+            display: inline-block;
+            margin-right: 14px;
+            cursor: pointer;
+            position: relative;
+          }
+          img {
+            height: 100px;
+          }
+          span {
+            position: absolute;
+            top: 0;
+            right: 0;
+            cursor: pointer;
+          }
+        `}</style>
+      </React.Fragment>
+    )
+  }
+}
