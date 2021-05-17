62eb13f94319f5625e545573176411a0d3e681e9
Tim Phillips <tim.p.phillips@gmail.com>
2021-05-16 18:44:17 -0700

    remove everything for v2

--- components/hoc/secure-page.js
+++
@@ -1,7 +0,0 @@
-import Page from './page'
-import ensureSignedIn from './ensure-signed-in'
-import compose from '../../lib/compose'
-
-// use the typical `Page` middleware and redirect to `/auth/sign-in`
-// when there's no session
-export default compose(Page, ensureSignedIn)
