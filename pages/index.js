bca5eb3d84ce1647000fc9616e9a463368c5f42f
JoshBurke <joshDgreat37@gmail.com>
2021-07-06 21:39:33 -0500

    add error-boundary

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import StoriesList from '../components/stories-list'
 import Page from '../components/hoc/page'
+import ErrorBoundary from '../components/error-boundary'
 
 function StoriesPage({
   openStory,
@@ -11,14 +12,16 @@ function StoriesPage({
   onStoryPlayClick
 }) {
   return (
-    <StoriesList
-      stories={[]}
-      openStory={openStory}
-      isPlaying={isPlaying}
-      playingStory={playingStory}
-      onStoryClick={onStoryClick}
-      onStoryPlayClick={onStoryPlayClick}
-    />
+    <ErrorBoundary>
+      <StoriesList
+        stories={[]}
+        openStory={openStory}
+        isPlaying={isPlaying}
+        playingStory={playingStory}
+        onStoryClick={onStoryClick}
+        onStoryPlayClick={onStoryPlayClick}
+      />
+    </ErrorBoundary>
   )
 }
 
