88e9f408377bb90ef9f60d676a2e97665ffdaf31
JoshBurke <joshDgreat37@gmail.com>
2021-07-10 14:03:10 -0500

    header styling

--- components/header.js
+++ components/header.js
@@ -11,31 +11,39 @@ const Header = () => (
     </h1>
 
     <nav>
+      *{'  '}
       <Link href="/">
         <a>Stories</a>
       </Link>
-
       <span> | </span>
-
       <Link href="/participate">
         <a>Participate</a>
       </Link>
-
       <span> | </span>
-
       <Link href="/about">
         <a>About</a>
       </Link>
+      {'  '}*
     </nav>
-
-    <br />
+    <hr />
 
     <style jsx>{`
+      .header {
+        top: 0px;
+        position: sticky;
+        background: rgb(239, 123, 126);
+        padding-top: 20px;
+        margin-top: -20px;
+      }
       img {
         width: 100%;
       }
       nav {
         padding-top: 5px;
+        font-size: 22px;
+      }
+      hr {
+        margin-left: -2px;
       }
     `}</style>
   </div>
