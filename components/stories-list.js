65ea12c367ac01cc1ee24a40912bbf79b8217fec
JoshBurke <joshDgreat37@gmail.com>
2021-08-28 19:55:47 -0700

    reset storieslist

--- components/stories-list.js
+++ components/stories-list.js
@@ -1,19 +1,9 @@
 import Story from '../components/story'
 
-export default function StoriesList({
-  stories,
-  isPlaying,
-  playingStory,
-  onStoryClick,
-}) {
+export default function StoriesList({ stories, onStoryClick }) {
   if (!stories || !stories.length) return <div>No stories here...</div>
 
   return stories.map((story) => (
-    <Story
-      key={story.id}
-      story={story}
-      isPlaying={isPlaying && story.id === playingStory.id}
-      onClick={onStoryClick}
-    />
+    <Story key={story.id} story={story} onClick={onStoryClick} />
   ))
 }
