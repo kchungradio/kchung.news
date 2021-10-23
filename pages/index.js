52e67f4f9192fa31c093045387805b48559f212d
JoshBurke <joshDgreat37@gmail.com>
2021-10-22 21:29:14 -0700

    add pagination to StoriesList

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import React, { useState, useEffect } from 'react'
 import StoriesList from '../components/stories-list'
 import Page from '../components/hoc/page'
 import ErrorBoundary from '../components/error-boundary'
-import { findStories } from '../lib/strapi-query'
+import { findStories, countStories } from '../lib/strapi-query'
+
+export const LIMIT_PER_PAGE = 20
 
 function StoriesPage({
   openStory,
@@ -13,10 +15,19 @@ function StoriesPage({
   onPlayClick,
 }) {
   const [stories, setStories] = useState([])
+  const [page, setPage] = useState(0)
+  const [numStories, setNumStories] = useState(-1)
 
   const findAndSetStories = async () => {
-    let response = await findStories()
+    setNumStories(await countStories())
+    let response = await findStories(page, LIMIT_PER_PAGE)
     setStories(response)
+  }
+
+  const goToPage = (n) => {
+    if (n >= 0 && n < numStories / LIMIT_PER_PAGE) {
+      setPage(n)
+    }
   }
 
   useEffect(() => {
@@ -32,6 +43,8 @@ function StoriesPage({
         playingStory={playingStory}
         onStoryClick={onStoryClick}
         onPlayClick={onPlayClick}
+        page={page}
+        goToPage={goToPage}
       />
     </ErrorBoundary>
   )
