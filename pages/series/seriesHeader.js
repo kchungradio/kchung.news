ae71bf7b6ed922e374d6d80253f5f40164023224
JoshBurke <joshDgreat37@gmail.com>
2021-12-05 18:56:28 -0800

    add truncation/show more to description for series

--- pages/series/seriesHeader.js
+++ pages/series/seriesHeader.js
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import { getSeriesByName } from '../../lib/strapi-query'
 
+// Num characters to truncate description to if it is 'collapsed'
+const MAX_DESCRIPTION_SIZE = 240
+const SHOW_MORE = 'Show More'
+const ELLIPSES = ' ... '
+
 function SeriesHeader({ seriesName }) {
   const [isLoading, setIsLoading] = useState(true)
+  const [collapsed, setCollapsed] = useState(true)
   const [series, setSeries] = useState({})
 
   const findAndSetSeries = async () => {
     let newSeries = await getSeriesByName(seriesName)
     setSeries(newSeries)
     setIsLoading(false)
+  }
+
+  const truncateDescription = (description) => {
+    if (description && description.length > MAX_DESCRIPTION_SIZE && collapsed) {
+      return description.slice(0, MAX_DESCRIPTION_SIZE)
+    }
+    return description
   }
 
   useEffect(() => {
@@ -20,7 +33,15 @@ function SeriesHeader({ seriesName }) {
       <>
         <div className="section-head">
           <h1>{`Series: ${seriesName}`}</h1>
-          <p className="description">{series?.description}</p>
+          <p className="description">
+            {truncateDescription(series?.description)}
+            {series?.description.length > MAX_DESCRIPTION_SIZE && collapsed && (
+              <>
+                <span>{ELLIPSES}</span>
+                <button onClick={() => setCollapsed(false)}>{SHOW_MORE}</button>
+              </>
+            )}
+          </p>
         </div>
         <style jsx>{`
           .section-head {
